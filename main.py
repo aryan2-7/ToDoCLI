81c1e7baa66d7005fc9991bac1763d4636a98351
Aryan Khatri <aryan9725@student.ku.edu.np>
2026-06-13 13:39:00 +0545

    setup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from todocli!")
+import click
 
+@click.command()
+@click.option("--name", prompt="Enter your name", help="The name to say hello to.")
+def hello(name):
+    click.echo(f"Hello {name}!")
 
 if __name__ == "__main__":
-    main()
+    hello()
